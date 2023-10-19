932aaacc2ae8e5668382ee99d9304de7b5ce0fd1
18Cygnus <aryasanjaya001@gmail.com>
2023-10-19 14:53:21 +0700

    adding a code where if "Forget Password?" textview is pressed in SignInActivity, an FP_1_enter_email popup appears

--- app/src/main/java/smt3/assignme_11/SignInActivity.kt
+++ app/src/main/java/smt3/assignme_11/SignInActivity.kt
@@ -1,35 +1,68 @@
 package smt3.assignme_11
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Button
 import android.widget.LinearLayout
+import android.app.Dialog
+import android.graphics.drawable.ColorDrawable
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import androidx.core.content.ContextCompat
+import android.view.WindowManager
+import android.view.Gravity
 
 class SignInActivity : AppCompatActivity() {
-    private lateinit var btnBackLogin : ImageView
     private lateinit var txtSignUp : TextView
+    private lateinit var txtLogin : TextView
+
+    lateinit var popupBtn: TextView
+    lateinit var mDialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
-        val linearLayout = findViewById<LinearLayout>(R.id.backButtonLogin)
-        val imageView = findViewById<ImageView>(R.id.backButtonImageLogin)
-        val pressedColor = ContextCompat.getColor(this, R.color.gray)
+        popupBtn = findViewById(R.id.txtforgotPassword)
+        mDialog = Dialog(this)
+        val pressedColor = ContextCompat.getColor(this, R.color.black_900_7f)
 
-        imageView.setOnClickListener {
-            imageView.setColorFilter(pressedColor)
-            val intent = Intent( this@SignInActivity, LandingActivity::class.java)
-            startActivity(intent)
+        popupBtn.setOnClickListener{
+            popupBtn.setTextColor(ContextCompat.getColorStateList(this, R.color.login_register))
+            val dialogView = layoutInflater.inflate(R.layout.activity_fp1_enter_email, null)
+            val window = mDialog.window
+            window?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this, R.color.black_semi_transparent)))
+            mDialog.setContentView(dialogView)
+
+            val imageViewCloseButton = dialogView.findViewById<ImageView>(R.id.closeButtonFpEmail)
+            imageViewCloseButton.setOnClickListener {
+                imageViewCloseButton.setColorFilter(pressedColor)
+                mDialog.dismiss()
+            }
+
+            txtLogin = dialogView.findViewById(R.id.txtLoginInFP)
+            txtLogin.setTextColor(ContextCompat.getColorStateList(this, R.color.login_fp_email))
+            txtLogin.setOnClickListener {
+                mDialog.dismiss()
+            }
+
+            val layoutParams = WindowManager.LayoutParams()
+            layoutParams.copyFrom(window?.attributes)
+            layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT
+            layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT
+            layoutParams.gravity = Gravity.CENTER
+            window?.attributes = layoutParams
+            mDialog.setCanceledOnTouchOutside(true)
+            mDialog.show()
         }
 
-        linearLayout.setOnClickListener {
+        val imageView = findViewById<ImageView>(R.id.backButtonImageLogin)
+
+        imageView.setOnClickListener {
             imageView.setColorFilter(pressedColor)
             val intent = Intent( this@SignInActivity, LandingActivity::class.java)
             startActivity(intent)
