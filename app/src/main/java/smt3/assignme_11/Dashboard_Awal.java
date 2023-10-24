bf45640c81b9ff612b8ac21e0c6bfd7df6ba910d
ardianti04 <ardiantisimanjuntak5@gmail.com>
2023-10-24 13:55:28 +0700

    menambahkan Dashboard_Awal

---
+++ app/src/main/java/smt3/assignme_11/Dashboard_Awal.java
@@ -0,0 +1,14 @@
+package smt3.assignme_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard_Awal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_awal);
+    }
+}
