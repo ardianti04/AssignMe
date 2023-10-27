7475e5a8823d403e6687a114d5263045a90491d2
ardianti04 <ardiantisimanjuntak5@gmail.com>
2023-10-27 15:48:29 +0700

    1. Menambahkan Dialog "Join Class". 2. Menghubungkan tombol "Tambah" pada All_class_RecView dengan JoinClassDialog, sehingga saat tombol "Tambah" ditekan, dialog "Join Class" muncul.

--- app/src/main/java/smt3/assignme_11/All_class_RecView.java
+++ app/src/main/java/smt3/assignme_11/All_class_RecView.java
@@ -1,20 +1,45 @@
 package smt3.assignme_11;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.ArrayList;
 
 public class All_class_RecView extends AppCompatActivity {
     private RecyclerView classRecView;
     private ClassRViewAdapter adapter;
+    private ImageButton btnTambah;
+    private AlertDialog dialog;
+    private Button btnCencel,btnJoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_class_rec_view);
+
+
+        btnTambah=findViewById(R.id.btnTambah);
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JoinClassDialog joinClassDialog = new JoinClassDialog(All_class_RecView.this);
+                joinClassDialog.showJoinClassDialog();
+            }
+        });
+
+
+
+
+
+
+
 
         adapter=new ClassRViewAdapter(this);
         classRecView=findViewById(R.id.classRecView);
@@ -36,4 +61,7 @@ public class All_class_RecView extends AppCompatActivity {
                 ,"Kelas untuk siswa ini saja","https://primaindisoft.com/wp-content/uploads/2019/09/ipa.png"));
         return kelas;
     }
+
+
+
 }
