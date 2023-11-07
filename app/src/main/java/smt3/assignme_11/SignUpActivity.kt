0161fc63099cc843acaa86fcbf3607c07a2dfb5f
ardianti04 <ardiantisimanjuntak5@gmail.com>
2023-11-07 14:49:36 +0700

    menambahkan warning jika data belum di isi

--- app/src/main/java/smt3/assignme_11/SignUpActivity.kt
+++ app/src/main/java/smt3/assignme_11/SignUpActivity.kt
@@ -1,32 +1,51 @@
 package smt3.assignme_11
 
+import AuthService
 import android.content.Intent
-import android.graphics.Typeface
 import android.os.Bundle
 import android.widget.ImageView
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.text.style.StyleSpan
-import android.util.Log
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.content.ContextCompat
-import smt3.assignme_11.SignInActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var btnBackLogin : ImageView
     private lateinit var txtSignIn : TextView
+    private lateinit var btnSignup:AppCompatButton
+    private lateinit var nameInputSignup: EditText
+    private lateinit var emailInputSignup: EditText
+    private lateinit var passwordInputSignup: EditText
+    private lateinit var ConfirmPasswordInputSignup: EditText
+
+
+
 
     
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        btnSignup = findViewById(R.id.btnSignUp)
+        nameInputSignup = findViewById(R.id.nameInputSignup)
+        emailInputSignup = findViewById(R.id.emailInputSignup)
+        passwordInputSignup = findViewById(R.id.passwordInputSignup)
+        ConfirmPasswordInputSignup = findViewById(R.id.ConfirmPasswordInputSignup)
+
+        btnSignup.setOnClickListener{
+            register()
+        }
+
 
         val linearLayout = findViewById<LinearLayout>(R.id.backButtonSignup)
         val imageView = findViewById<ImageView>(R.id.backButtonImageSignup)
@@ -56,4 +75,25 @@ class SignUpActivity : AppCompatActivity() {
 
 
     }
+
+     fun register() {
+        if (nameInputSignup.text.isEmpty()) {
+            nameInputSignup.error = "The name column cannot be empty"
+            nameInputSignup.requestFocus()
+            return
+        } else if (emailInputSignup.text.isEmpty()) {
+            emailInputSignup.error = "The email column cannot be empty"
+            emailInputSignup.requestFocus()
+            return
+        } else if (passwordInputSignup.text.isEmpty()) {
+            passwordInputSignup.error = "The password column cannot be empty"
+            passwordInputSignup.requestFocus()
+            return
+        } else if (ConfirmPasswordInputSignup.text.isEmpty()) {
+            ConfirmPasswordInputSignup.error = "The Confirm Password column cannot be empty"
+            ConfirmPasswordInputSignup.requestFocus()
+            return
+        }
+    }
+
 }
