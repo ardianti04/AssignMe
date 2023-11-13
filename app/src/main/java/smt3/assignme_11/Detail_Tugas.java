8ad7377408a5381a5314913bcc8a1e59967ba639
ardianti04 <ardiantisimanjuntak5@gmail.com>
2023-11-13 16:05:29 +0700

    Detail_Tugas.java

---
+++ app/src/main/java/smt3/assignme_11/Detail_Tugas.java
@@ -0,0 +1,16 @@
+package smt3.assignme_11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import smt3.assignme_11.R;
+
+public class Detail_Tugas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_tugas);
+    }
+}
