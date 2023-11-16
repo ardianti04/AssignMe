9d2ed62927114fe6a0af47b1661b7c36378ea308
ardianti04 <ardiantisimanjuntak5@gmail.com>
2023-11-16 21:30:40 +0700

    Menghubungkan Halaman Detail_Kelas dengan fragmen dk_Material, dk_Task, dk_people

---
+++ app/src/main/java/detail_kelas/Detail_Kelas.kt
@@ -0,0 +1,12 @@
+package detail_kelas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import smt3.assignme_11.R
+
+class Detail_Kelas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_kelas)
+    }
+}
